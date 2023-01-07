b8835fe4134ebacdf87a648bdc9bc576957f1008
Akin <>
2023-01-07 11:23:28 +0100

    install webpack package

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./lib/components/Index.jsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
